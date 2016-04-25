2ccd5691fe522ff5e9882a9db4b3e0583abe2b43
TJQKAS <A26052013@GMAIL.COM>
2016-04-25 22:28:13 +0100

    Layouts and routes

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample Application"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample Application"
+  end
 end

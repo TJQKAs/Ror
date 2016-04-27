ce52ccf3711ea0004508b5ad05cfce077700164d
TJQKAS <A26052013@GMAIL.COM>
2016-04-28 00:05:37 +0100

    sample_app create db with attributes and passw

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  #we want that all emails will be converted to lowercase view before we
+  #save it in data base
+  before_save {self.email = email.downcase}
+  # checks that we have name
+  validates  :name,            presence: true, length: {maximum: 30}
+  validates  :nickname,     presence: true, length: {maximum: 255}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\z/
+  validates  :email,            presence: true, length: {maximum: 30, minimum: 5},
+                                         format: {with: VALID_EMAIL_REGEX},
+                                         uniqueness: {case_sensetive: false}
+  #add functionality for secure password
+  has_secure_password
+  validates  :password,  length: {minimum: 8}
 end

efce8a2661bd5c38d5a6bed96df00eb715993984
TJQKAS <A26052013@GMAIL.COM>
2016-04-30 22:22:40 +0100

    integration test for signup

--- sample_app/test/integration/users_signup_test.rb
+++ sample_app/test/integration/users_signup_test.rb
@@ -12,14 +12,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   #       assert_template 'users/new'
   # end
 
-
 test "invalid signup information" do
       get signup_path
       # assert that the count of users will not change because we aren't able to create user with the data below
           assert_no_difference "User.count" do
-          post users_path, user: {name: "", nickname: "", email: "user_invalid", password: "abs", password_confirmation: "cde" }
+          post users_path, user: {name: "", nickname: "", email: "user_invalid",
+          password: "abs", password_confirmation: "cde" }
           end
       assert_template 'users/new'
+end
+
+test "valid signup information" do
+      get signup_path
+      # assert that the count of users will not change because we aren't able to create user with the data below
+          assert_difference "User.count", 1  do
+          post_via_redirect users_path, user: {name: "James", nickname: "Trumble", email: "Jtrumble@gmail.com",
+          password: "123456789", password_confirmation: "123456789" }
+          end
+      # follow_redirect!
+      assert_template 'users/show'
 end
 
 
